a96a937073997c16fb9c2f526fa008c4ccb1da8e
erickearaujo <erickedu.araujo@gmail.com>
2022-04-20 01:32:46 -0300

    inicio

---
+++ Caneta.php
@@ -0,0 +1,33 @@
+<?php
+class Caneta{
+    var $modelo;
+    var $cor;
+    var $ponta;
+    var $carga;
+    var $tampada;
+
+
+
+function rabiscar(){
+
+}
+
+function tampar(){
+    
+
+}
+
+
+function destampar(){
+
+}
+
+
+
+
+
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aulaphp2</title>
+</head>
+<body>
+    <?php
+require_once 'Caneta.php';
+
+        $c1= new Caneta;
+        $c1->cor = "azul";
+        $c1->ponta = 0.5;
+        $c1->tampada = false;
+
+
+
+    
+    ?>
+    
+</body>
+</html>
